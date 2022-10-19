b7c158a3edccbcac17b21569fc16f1de5443a0a3
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-19 14:42:37 -0400

    javascript

---
+++ javascripts/aashna.js
@@ -0,0 +1,6 @@
+textFunction(){
+    const unordered = document.querySelector('ul');
+    const text = document.createElement('p');
+    text.textContent = "You pressed a button!";
+    unordered.appendChild(text);
+}
