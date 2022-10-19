b25f3f1a977f525393b7a6fdc1abdbdd3a4b89c0
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-19 14:47:15 -0400

    java

--- javascripts/aashna.js
+++ javascripts/aashna.js
@@ -1,5 +1,5 @@
-button1.onclick = textFunction () {
-    const unordered = document.querySelector('ul');
+function textFunction() {
+    const unordered = document.querySelector('.ul');
     const text = document.createElement('p');
     text.textContent = "You pressed a button!";
     unordered.appendChild(text);
