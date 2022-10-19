b34be7c340b745b8305adf822b549088f295f5eb
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-19 15:09:40 -0400

    java

--- javascripts/aashna.js
+++ javascripts/aashna.js
@@ -1,7 +1,7 @@
 const para = document.createElement("p");
 const element = document.querySelector("body");
 
-#button1.onclick = textFunction() {
+#button1.onclick = textFunction(){
     para.textContent = "You pressed a button!";
     element.appendChild(para);
 };
