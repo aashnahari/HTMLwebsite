77c6ff4a9cac7f7d003e0901f2092daf103a25df
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-21 15:09:48 -0400

    java

--- javascripts/aashna.js
+++ javascripts/aashna.js
@@ -1,10 +1,11 @@
-const button1 = document.createElement('button')
-button1.innerText = 'Press me!'
+const button = document.createElement('button')
+button.innerText = 'Press me!'
 
-button1.addEventListener('click', () => {
+ 
+button.addEventListener('click', () => {
     alert('You clicked the button!')
 })
+document.body.appendChild(button)
 
-document.body.appendChild(button1)
 
 
