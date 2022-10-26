4059a2e4486b70b341252bebb6f420727483aac1
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-26 14:54:50 -0400

    button

--- javascripts/aashna.js
+++ javascripts/aashna.js
@@ -1,11 +1,28 @@
 const button1 = document.createElement('button')
 button1.innerText = 'Press me!'
 
+const button2 = document.createElement('button')
+button2.innerText = 'Press me too!'
+
  
 button1.addEventListener('click', () => {
-    alert('You clicked the button!')
+
+    const text1 = document.createElement('p')
+    text1.innerText = 'You clicked the button!'
+    document.body.appendChild(text1)
 })
 document.body.appendChild(button1)
 
 
+button2.addEventListener('click', () => {
 
+    var heading = document.h4;
+    var style = window.getComputedStyle(heading, null).getPropertyValue('font-size');
+    var currentSize = parseInt(style);
+    currentSize++;
+    document.h4.style.fontSize = currentSize.toString();
+
+})
+
+
+document.body.appendChild(button2)
