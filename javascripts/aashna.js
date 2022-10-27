7a096f3be582ed5ea240386e8f99bf388699ff73
Aashna Hari <aashnahari@Aashnas-MacBook-Pro.local>
2022-10-27 09:48:19 -0400

    color

--- javascripts/aashna.js
+++ javascripts/aashna.js
@@ -4,6 +4,16 @@ button1.innerText = 'Press me!'
 const button2 = document.createElement('button')
 button2.innerText = 'Press me too!'
 
+const htmlEle = document.getElementById("number")
+let num = 0
+
+const color1 = document.createElement('button')
+color1.innerText = 'Pink'
+const color2 = document.createElement('button')
+color2.innerText = 'Purple'
+const color3 = document.createElement('button')
+color3.innerText = 'Blue'
+
  
 button1.addEventListener('click', () => {
 
@@ -15,14 +25,38 @@ document.body.appendChild(button1)
 
 
 button2.addEventListener('click', () => {
-
-    var heading = document.h4;
-    var style = window.getComputedStyle(heading, null).getPropertyValue('font-size');
-    var currentSize = parseInt(style);
-    currentSize++;
-    document.h4.style.fontSize = currentSize.toString();
-
+    htmlEle.innerText = num++
+    
 })
 
 
+
 document.body.appendChild(button2)
+
+
+color1.addEventListener('click', () => {
+
+    const text2 = document.getElementById("color")
+    text2.style.color = "pink"
+
+
+})
+document.body.appendChild(color1)
+
+color2.addEventListener('click', () => {
+
+    const text2 = document.getElementById("color")
+    text2.style.color = "purple"
+
+
+})
+document.body.appendChild(color2)
+
+color3.addEventListener('click', () => {
+
+    const text2 = document.getElementById("color")
+    text2.style.color = "blue"
+
+
+})
+document.body.appendChild(color3)
